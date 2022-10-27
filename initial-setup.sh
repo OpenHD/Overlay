7be38dcc08143646b66944a5535b6142ffd7edaa
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-10-27 17:14:37 +0200

    Create initial-setup.sh

---
+++ initial-setup.sh
@@ -0,0 +1,26 @@
+echo ""
+n=" ██████╗ ██████╗ ███████╗███╗   ██╗   ██╗  ██╗██████╗   " && echo "${n::${COLUMNS:-$(tput cols)}}" # some magic to cut the end on smaller terminals
+n="██╔═══██╗██╔══██╗██╔════╝████╗  ██║   ██║  ██║██╔══██╗  " && echo "${n::${COLUMNS:-$(tput cols)}}"
+n="██║   ██║██████╔╝█████╗  ██╔██╗ ██║   ███████║██║  ██║  " && echo "${n::${COLUMNS:-$(tput cols)}}"
+n="██║   ██║██╔═══╝ ██╔══╝  ██║╚██╗██║   ██╔══██║██║  ██║  " && echo "${n::${COLUMNS:-$(tput cols)}}"
+n="╚██████╔╝██║     ███████╗██║ ╚████║██╗██║  ██║██████╔╝  " && echo "${n::${COLUMNS:-$(tput cols)}}"
+n=" ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝╚═════╝   " && echo "${n::${COLUMNS:-$(tput cols)}}"
+echo ""
+
+echo "Please Stand by, this script will install additional drivers"
+
+echo ""
+
+echo "this will take some time, please do not restart your computer or stop this script"
+
+echo ""
+
+echo "we're now installing the rtl8812au driver"
+
+cd /opt/X86/rtl8812au
+./dkms-install.sh
+
+echo "After a reboot OpenHD should be usable now"
+
+
+
