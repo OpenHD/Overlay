1374a33e23c1f6500ceb64de664c9499c8eb5f96
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-11-04 02:09:24 +0100

    Update initial-setup.sh

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo ""
 n=" ██████╗ ██████╗ ███████╗███╗   ██╗   ██╗  ██╗██████╗   " && echo "${n::${COLUMNS:-$(tput cols)}}" # some magic to cut the end on smaller terminals
 n="██╔═══██╗██╔══██╗██╔════╝████╗  ██║   ██║  ██║██╔══██╗  " && echo "${n::${COLUMNS:-$(tput cols)}}"
@@ -18,7 +19,7 @@ echo ""
 echo "we're now installing the rtl8812au driver"
 
 cd /opt/X86/rtl8812au-5.2.20
-./dkms-install.sh
+./dkms-install.sh 2> /opt/X86/install.log
 
 echo "After a reboot OpenHD should be usable now"
 rm -rf /opt/X86/initial-setup.sh
